4f21e191627d75e4b201e0eb0f3133c2449e6764
Balaji Singaram <41104432+balajisingaram@users.noreply.github.com>
2026-03-16 21:39:54 +0530

    Add files via upload

---
+++ workhub-app/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.pravatar.cc', 'images.unsplash.com', 'api.dicebear.com'],
+  },
+}
+
+module.exports = nextConfig
